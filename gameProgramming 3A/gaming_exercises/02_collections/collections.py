004c53003da23438f939d12e3ffc73e430a1fdbf
s11060712 <111775682+s11060712@users.noreply.github.com>
2023-10-05 11:42:28 -0400

    v0.2

--- gameProgramming 3A/gaming_exercises/02_collections/collections.py
+++ gameProgramming 3A/gaming_exercises/02_collections/collections.py
@@ -1,17 +1,29 @@
-# Collections, Kloeckner Gavin, v0.1.1
+# Collections, Kloeckner Gavin, v0.2
 # Adding items
 
-playerInventory = []
-while len(playerInventory) < 10:
-    item = input("What would you like to add?\n")
-    playerInventory.append(item)
-playerInventory.sort()
-# .whatever() is known as a method. It means "do this to that".
-print(playerInventory)
+# playerInventory = []
+# while len(playerInventory) < 10:
+#     item = input("What would you like to add?\n")
+#     playerInventory.append(item)
+# playerInventory.sort()
+# # .whatever() is known as a method. It means "do this to that".
+# print(playerInventory)
 
-# Removing Items
-while len(playerInventory) > 5:
-    item = input("Now, please select the item that you would like to remove from the inventory.\n")
-    playerInventory.remove(item)
-playerInventory.sort()
-print(playerInventory)
+# # Removing Items
+# while len(playerInventory) > 5:
+#     item = input("Now, please select the item that you would like to remove from the inventory.\n")
+#     playerInventory.remove(item)
+# playerInventory.sort()
+# print(playerInventory)
+
+weaponList = [
+    False, # Crytinan Standard Issue RaL-TC
+    False, # Personal Radiation Pistol-17 (PRP-17)
+    False, # V3 Impulse Grenade
+    False, # Energy Shield
+    False  # ARK-56 (Flame Launcher)
+]
+# each item in the list is called an element, the location of each element is called an
+#index
+# first element is index[0]
+# shortcut to the last element is index[-1]
