2bfb0b823bdb9db7ddd0874882086f5e3949f5a7
s11060712 <111775682+s11060712@users.noreply.github.com>
2023-10-18 11:51:42 -0400

    v0.1

--- gameProgramming 3A/gaming_exercises/03_functions/functions.py
+++ gameProgramming 3A/gaming_exercises/03_functions/functions.py
@@ -1,4 +1,4 @@
-# Functions, 10-18-23, Gavin Kloeckner, v0.0
+# Functions, 10-18-23, Gavin Kloeckner, v0.1
 
 def functionName(): # Function Signature 
     print("What is your name?\n")
@@ -7,3 +7,16 @@ def functionName(): # Function Signature
 
 # Call The Function
 functionName()
+
+def happyBirthday(numTimes, age):
+    count = 0
+    while count < numTimes:
+        print("Happy Birthday!\n")
+    count += 1
+    print(f"Congratulations on turning {age}-years-old!\n")
+
+
+happyBirthday(10, 36)
+happyBirthday(5, 17)
+happyBirthday(16, 15)
+happyBirthday(6, 14)
