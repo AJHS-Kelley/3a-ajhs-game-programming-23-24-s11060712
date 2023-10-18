a1fbf297edc42dd2bfc3d3bcf41583699edfc4b0
s11060712 <111775682+s11060712@users.noreply.github.com>
2023-10-18 13:04:42 -0400

    v0.3

--- gameProgramming 3A/gaming_exercises/03_functions/functions.py
+++ gameProgramming 3A/gaming_exercises/03_functions/functions.py
@@ -1,33 +1,53 @@
-# Functions, 10-18-23, Gavin Kloeckner, v0.2
+# Functions, 10-18-23, Gavin Kloeckner, v0.3
 
-def functionName(): # Function Signature 
-    print("What is your name?\n")
-    name = input("Please type your name in the space below.\n")
-    print(f"Hello there {name}!\n")
+import random
 
-# Call The Function
-functionName()
+# def functionName(): # Function Signature 
+#     print("What is your name?\n")
+#     name = input("Please type your name in the space below.\n")
+#     print(f"Hello there {name}!\n")
 
-def happyBirthday(numTimes, age):
+# # Call The Function
+# functionName()
+
+# def happyBirthday(numTimes, age):
+#     count = 0
+#     while count < numTimes:
+#         print("Happy Birthday!\n")
+#     count += 1
+#     print(f"Congratulations on turning {age}-years-old!\n")
+
+
+# #happyBirthday(10, 36)
+
+# def functionWithReturn(num1, num2):
+#     sum = num1 + num2
+#     quotient = sum / 5
+#     return quotient # returns immediately exits a function
+
+# def functionWithReturn(num1, num2):
+#     sum = num1 + num2
+#     quotient = sum / 5
+#     print(quotient)
+
+# example = functionWithReturn(4, 17)
+# print(example)
+
+def rollDice(numRoll, sizeRoll):
     count = 0
-    while count < numTimes:
-        print("Happy Birthday!\n")
-    count += 1
-    print(f"Congratulations on turning {age}-years-old!\n")
+    sum = 0
+    while count < numRoll:
+        roll = random.randint(1, sizeRoll)
+        # print(f"Roll #{count}: {roll}\n")
+        sum += roll
+        count += 1
+    # print(sum)
+    return sum
 
+print("D4 Rolls")
+d4Sum = rollDice(10, 4)
+print("D20 Rolls")
+d20Sum = rollDice(2, 20)
 
-#happyBirthday(10, 36)
-
-def functionWithReturn(num1, num2):
-    sum = num1 + num2
-    quotient = sum / 5
-    return quotient # returns immediately exits a function
-
-def functionWithReturn(num1, num2):
-    sum = num1 + num2
-    quotient = sum / 5
-    print(quotient)
-
-example = functionWithReturn(4, 17)
-print(example)
-
+print(d4Sum)
+print(d20Sum)
