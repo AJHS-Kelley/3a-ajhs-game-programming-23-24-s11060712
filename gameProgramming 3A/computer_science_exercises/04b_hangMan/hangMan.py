51152efd53aba65aed0b99553421472c8bd51adc
s11060712 <111775682+s11060712@users.noreply.github.com>
2023-11-01 12:05:40 -0400

    v0.1.1

--- gameProgramming 3A/computer_science_exercises/04b_hangMan/hangMan.py
+++ gameProgramming 3A/computer_science_exercises/04b_hangMan/hangMan.py
@@ -1,3 +1,4 @@
+import random
 wordList = 'cat fish dog car hippo tail red blue green cyan make break tall small real meal krill teal cow moose'.split()
 # .split() will split a string into separate elements, by default based on SPACE
 
@@ -45,8 +46,11 @@ HANGMAN_BOARD = ['''
    / \    |   
           |   
         =====''']                                           
-            
-i = 0            
-while i < len(HANGMAN_BOARD):
-    print(HANGMAN_BOARD[i])
-    i += 1
+
+def getRandomWord(wordList):
+    pass
+
+# i = 0            
+# while i < len(HANGMAN_BOARD):
+#     print(HANGMAN_BOARD[i])
+#     i += 1
