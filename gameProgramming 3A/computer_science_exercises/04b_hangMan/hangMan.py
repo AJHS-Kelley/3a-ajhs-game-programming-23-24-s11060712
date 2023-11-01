8c96b0d7713b5aaf6924946c9809d7a7fc56bcec
s11060712 <111775682+s11060712@users.noreply.github.com>
2023-11-01 11:40:43 -0400

    v0.0.1

--- gameProgramming 3A/computer_science_exercises/04b_hangMan/hangMan.py
+++ gameProgramming 3A/computer_science_exercises/04b_hangMan/hangMan.py
@@ -1,3 +1,2 @@
-wordList = ['cat dog fish car'
-
-]
+wordList = 'cat fish dog car hippo tail red blue green cyan make break tall small real meal krill teal cow moose'.split()
+# .split() will split a string into separate elements, by default based on SPACE
