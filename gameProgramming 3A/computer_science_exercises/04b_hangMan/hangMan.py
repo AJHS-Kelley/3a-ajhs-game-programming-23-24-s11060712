f38dc6f4e649b2674b24809589857cef6a5defeb
s11060712 <111775682+s11060712@users.noreply.github.com>
2023-11-01 11:59:54 -0400

    v0.1

--- gameProgramming 3A/computer_science_exercises/04b_hangMan/hangMan.py
+++ gameProgramming 3A/computer_science_exercises/04b_hangMan/hangMan.py
@@ -1,2 +1,52 @@
 wordList = 'cat fish dog car hippo tail red blue green cyan make break tall small real meal krill teal cow moose'.split()
 # .split() will split a string into separate elements, by default based on SPACE
+
+# VARIABLE_NAMES that are all caps are constants and not meant to change
+HANGMAN_BOARD = ['''
+    +-----+
+          |  
+          |   
+          |   
+          |   
+        =====''', '''     
+    +-----+
+    O     |  
+          |   
+          |   
+          |   
+        =====''', '''             
+    +-----+
+    O     |  
+    |     |   
+          |   
+          |   
+        =====''', '''         
+    +-----+
+    O     |  
+    |\    |   
+          |   
+          |   
+        =====''', '''         
+    +-----+
+    O     |  
+   /|\    |   
+          |   
+          |   
+        =====''', '''             
+    +-----+
+    O     |  
+   /|\    |   
+   /      |   
+          |   
+        =====''', '''             
+    +-----+
+    O     |  
+   /|\    |   
+   / \    |   
+          |   
+        =====''']                                           
+            
+i = 0            
+while i < len(HANGMAN_BOARD):
+    print(HANGMAN_BOARD[i])
+    i += 1
