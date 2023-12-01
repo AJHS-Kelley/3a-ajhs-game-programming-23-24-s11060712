24ea11c440327ebb023bba90a016b8cb71b57c96
s11060712 <111775682+s11060712@users.noreply.github.com>
2023-12-01 11:46:27 -0500

    v0.1.4

--- gameProgramming 3A/computer_science_exercises/05a_exampleGameFunctions/exampleGameFunctions.py
+++ gameProgramming 3A/computer_science_exercises/05a_exampleGameFunctions/exampleGameFunctions.py
@@ -1,4 +1,4 @@
-# Example Game Functions, Gavin Kloeckner, v0.1.3
+# Example Game Functions, Gavin Kloeckner, v0.1.4
 import random
 
 damageNum = 0
@@ -10,34 +10,34 @@ cardType = 'Infantry Artillery Tank Aerial'.split()
 teamSelect = ['Team 1', 'Team 2', 'Team 3', 'Team 4']
 
 
-def getTeam():
-    print('Welcome to the Intergelactic Card Game!\n')
-    team = input(f'Before we get started, please input a number of the team you would like to be on:\n{teamSelect}\n')
-    while True:
-        if team not in '1234':
-            team = input('Team number not in range. Please input a number within 1-4 and press enter.\n')
-        elif team in '12345':
-            clarify = input(f'Are you sure you want to be on {team}?\n')
-        if clarify == 'yes':
-            print('Alright, now that you have selected your team,\nlet\'s move on.')
-            break
-        else:
-            team = input(f'Please select what team you want to be on:\n{teamSelect}\n')
+# def getTeam():
+#     print('Welcome to the Intergelactic Card Game!\n')
+#     team = input(f'Before we get started, please input a number of the team you would like to be on:\n{teamSelect}\n')
+#     while True:
+#         if team not in '1234':
+#             team = input('Team number not in range. Please input a number within 1-4 and press enter.\n')
+#         elif team in '12345':
+#             clarify = input(f'Are you sure you want to be on {team}?\n')
+#         if clarify == 'yes':
+#             print('Alright, now that you have selected your team,\nlet\'s move on.')
+#             break
+#         else:
+#             team = input(f'Please select what team you want to be on:\n{teamSelect}\n')
 
-getTeam()
+# getTeam()
 
-# def cardStatus(cardType, cardAmount):
-#     while len(cardAmount) < 10:
-#         cardType = cardType[random.randint(0,4)]
-#         damageNum = damageNum[random.randint(0,50)]
-#         moveSpeed = moveSpeed[random.randint(0,50)]
-#         armorStat = armorStat[random.randint(0,10)]
-#         baseHealth = baseHealth[random.randint(0,100)]
-#         deck = cardType + " " + damageNum + " " + moveSpeed + " " + armorStat + " " + baseHealth
-#         cardType += deck
-#         cardAmount.append(deck)
+def cardStatus(cardType, cardAmount):
+    while cardAmount < 10:
+        cardType = cardType[random.randint(0,3)]
+        damageNum = random.randint(0,50)
+        moveSpeed = random.randint(0,50)
+        armorStat = random.randint(0,10)
+        baseHealth = random.randint(0,100)
+        deck = cardType + damageNum + moveSpeed + armorStat + baseHealth
+        cardType += deck
+        cardAmount.append(deck)
 
-# cardStatus(cardType, cardAmount)
+cardStatus(cardType, cardAmount)
 
 
 # Code Review by Lily King
