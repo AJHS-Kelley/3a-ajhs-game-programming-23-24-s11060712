a0c63c26f0103f8b09a6c238486f9e363cda8a6e
Ryan Kelley <kelleyr@duvalschools.org>
2023-12-05 12:00:45 -0500

    Code Review Test -- 12-05-23

--- gameProgramming 3A/computer_science_exercises/05a_exampleGameFunctions/exampleGameFunctions.py
+++ gameProgramming 3A/computer_science_exercises/05a_exampleGameFunctions/exampleGameFunctions.py
@@ -15,7 +15,7 @@ cardType = [
 teamSelect = ['Fake Team', 'Team 1', 'Team 2', 'Team 3', 'Team 4']
 
 
-def getTeam():
+def getTeam(): # Function passes code test.  
     print('Welcome to the Intergalactic Card Game!\n')
     team = input(f'Before we get started, please input a number of the team you would like to be on:\n{teamSelect}\n')
     
@@ -27,19 +27,54 @@ def getTeam():
 
 myTeam = getTeam()
 
-cardtype = []
-def cardStatus(cardType):
-    while len(cardType) < 10:
-        cardType = [random.randint(0, 4)]
+def cardStatus(cardType): # Finish the if/elf/else block for the card types. 
+    # while len(cardType) < 10:
+    #     cardType = [random.randint(0, 4)]
+    #     damageNumGen = random.randint(0, 50)
+    #     moveSpeedGen = random.randint(0, 50)
+    #     armorStatGen = random.randint(0, 10)
+    #     baseHealthGen = random.randint(0, 100)
+        
+        
+
+    if cardType == 'Artillery':
         damageNumGen = random.randint(0, 50)
         moveSpeedGen = random.randint(0, 50)
         armorStatGen = random.randint(0, 10)
         baseHealthGen = random.randint(0, 100)
-        deck = (cardType,damageNumGen, moveSpeedGen, armorStatGen, baseHealthGen)
-        
-        print(deck)
+    elif cardType == 'Infantry':
+        damageNumGen = random.randint(0, 10)
+        moveSpeedGen = random.randint(0, 10)
+        armorStatGen = random.randint(0, 5)
+        baseHealthGen = random.randint(0, 10)
+    # elif Tank 
+    # elif Ariel 
+    else: 
+        print('Error Message Here')
+    card = (cardType,damageNumGen, moveSpeedGen, armorStatGen, baseHealthGen)
+    print(card)
 
-#cardStatus(cardType)
+cardStatus('Artillery')
+cardStatus('Infantry')
+
+# while numCardsInDeck > 0: 
+#     drawCard()
+#     cardType = getCardType(card)
+#     cardStatus(cardType) 
+
+def makeDeck(cardType): 
+    deckList = [] 
+    while len(deckList) < 10: 
+        deckList.append(cardType[random.randint(0, 3)]) 
+    print(deckList)
+
+makeDeck(cardType)
+
+def shuffleDeck(cardList):
+    pass
+
+def dealCards(numCards, numPlayers): 
+    pass
 
 
 # Code Review by Lily King
