ac31aa42f13cac532c4abb1cc33917dccbce638e
s11060712 <111775682+s11060712@users.noreply.github.com>
2023-12-11 12:03:56 -0500

    v0.4

--- gameProgramming 3A/computer_science_exercises/06a_classesObjects/classesObjects.py
+++ gameProgramming 3A/computer_science_exercises/06a_classesObjects/classesObjects.py
@@ -1,4 +1,4 @@
-# Classes and Objects, Gavin Kloeckner, v0.3.1
+# Classes and Objects, Gavin Kloeckner, v0.4
 
 class Person: # Class names should be PascalCase
     def __init__(self, age, height, hairColor, name, weight, birthday):
@@ -14,18 +14,39 @@ class Person: # Class names should be PascalCase
     def __str__(self):
         return f"{self.name} is {self.age} years old.\nThey are {self.height} and their birthday is {self.birthday}.\nThey weigh {self.weight} lbs and have {self.hairColor} color hair.\n"
 
+    # Functions in a Class
+    def tooOld(self):
+        print('Hello, I will determine if you are too old to use this program.\n')
+        print('You must be uder 55 to use this program.\n')
+        if self.age > 20:
+            print('You are too old to use this program.\n')
+        else:
+            print('You are clear to use this program.\n')
+
+    def needDiet(self):
+        print('Hello, this program will determine if you need to go on a diet or not.\n')
+        print('You must be under 300lbs.\n')
+        if self.weight > 300:
+            print('You need to go on a diet.\n')
+        else:
+            print('You\'re ok for the time being.\n')
+
 
 
 # A class is a 'blueprint' to make an object
 
 examplePerson = Person(17, '5\'9\"', 'brown', 'Gavin', 122, 'July 17')
 examplePerson2 = Person(31, '6\'1\"', 'black', 'John', 200, 'April 31')
-print(examplePerson)
 
-# Changing Properties After Creating Object
-print(examplePerson2.age)
-examplePerson2.age = 320
-print(examplePerson2.age)
-print(examplePerson2.birthday)
-examplePerson2.birthday = "March 05"
-print(examplePerson2.birthday)
+examplePerson.tooOld()
+examplePerson.needDiet()
+
+# print(examplePerson)
+
+# # Changing Properties After Creating Object
+# print(examplePerson2.age)
+# examplePerson2.age = 320
+# print(examplePerson2.age)
+# print(examplePerson2.birthday)
+# examplePerson2.birthday = "March 05"
+# print(examplePerson2.birthday)
