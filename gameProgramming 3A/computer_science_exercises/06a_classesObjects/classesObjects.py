e0f2279a7cbe9667a2e030ca1fdf1282a4be3bfc
s11060712 <111775682+s11060712@users.noreply.github.com>
2023-12-11 11:39:34 -0500

    v0.2

--- gameProgramming 3A/computer_science_exercises/06a_classesObjects/classesObjects.py
+++ gameProgramming 3A/computer_science_exercises/06a_classesObjects/classesObjects.py
@@ -1,4 +1,4 @@
-# Classes and Objects, Gavin Kloeckner, v0.1
+# Classes and Objects, Gavin Kloeckner, v0.2
 
 class Person: # Class names should be PascalCase
     def __init__(self, age, height, hairColor, name, weight, birthday):
@@ -9,3 +9,16 @@ class Person: # Class names should be PascalCase
         self.name = name
         self.weight = weight
         self.birthday = birthday
+
+
+# A class is a 'blueprint' to make an object
+
+examplePerson = Person(17, '5\'9\"', 'brown', 'Gavin', 122, 'July 17')
+examplePerson2 = Person(31, '6\'1\"', 'black', 'John', 200, 'April 31')
+print(examplePerson.age)
+print(examplePerson.height)
+print(examplePerson.name)
+
+print(examplePerson2.age)
+print(examplePerson2.height)
+print(examplePerson2.name)
