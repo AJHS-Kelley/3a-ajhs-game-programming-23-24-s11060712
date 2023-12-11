0842fd068a8a130c20b81f588a5e29783df8b7d4
s11060712 <111775682+s11060712@users.noreply.github.com>
2023-12-11 11:31:20 -0500

    v0.1

--- gameProgramming 3A/computer_science_exercises/06a_classesObjects/classesObjects.py
+++ gameProgramming 3A/computer_science_exercises/06a_classesObjects/classesObjects.py
@@ -1,1 +1,11 @@
-# Classes and Objects, Gavin Kloeckner, v0.0
+# Classes and Objects, Gavin Kloeckner, v0.1
+
+class Person: # Class names should be PascalCase
+    def __init__(self, age, height, hairColor, name, weight, birthday):
+        # The self keyword refers to the specific object that is being delt with
+        self.age = age
+        self.height = height
+        self.hairColor = hairColor
+        self.name = name
+        self.weight = weight
+        self.birthday = birthday
