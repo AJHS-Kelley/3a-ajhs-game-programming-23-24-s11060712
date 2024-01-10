c316363b3d08b27f2fe45d569545c65e6daeceed
s11060712 <111775682+s11060712@users.noreply.github.com>
2024-01-10 13:02:43 -0500

    v0.2a

--- gameProgramming 3A/cohort_lessons/dnaGame.py
+++ gameProgramming 3A/cohort_lessons/dnaGame.py
@@ -1,4 +1,4 @@
-# DNA replication Gavin Kloeckner, v0.1a
+# DNA replication Gavin Kloeckner, v0.2a
 
 import time, datetime # Bring the whole tool box
 
@@ -7,3 +7,19 @@ import time, datetime # Bring the whole tool box
 from random import choice # Bring just the tool needed
 
 dnaBase = ["A", "T", "G", "C"] # Adnine, Thymine, Guanine, Cytosine
+
+def gameIntro() -> None:
+    pass
+
+def genDNA() -> str:
+    basesGenerated = 0
+    basesRequested = int(input("Please enter a positive integer value for the number of bases you want generated.\n"))
+    dnaSequence = ""
+
+    while basesGenerated < basesRequested:
+        dnaSequence += choice(dnaBase)
+        basesGenerated += 1
+    return dnaSequence
+
+dna = genDNA()
+print(dna)
