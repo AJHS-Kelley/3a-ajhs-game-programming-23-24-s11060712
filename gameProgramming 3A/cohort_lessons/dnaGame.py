83dbe5ee41c9fad5b435847d96e658997840bbc1
s11060712 <111775682+s11060712@users.noreply.github.com>
2024-01-10 12:05:34 -0500

    v0.1a

---
+++ gameProgramming 3A/cohort_lessons/dnaGame.py
@@ -0,0 +1,9 @@
+# DNA replication Gavin Kloeckner, v0.1a
+
+import time, datetime # Bring the whole tool box
+
+
+# Import specific method from a module
+from random import choice # Bring just the tool needed
+
+dnaBase = ["A", "T", "G", "C"] # Adnine, Thymine, Guanine, Cytosine
