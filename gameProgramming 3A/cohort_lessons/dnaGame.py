b7e389ec19d2012d57367ac27b3d492b65479e90
s11060712 <111775682+s11060712@users.noreply.github.com>
2024-01-10 13:20:01 -0500

    v0.3a

--- gameProgramming 3A/cohort_lessons/dnaGame.py
+++ gameProgramming 3A/cohort_lessons/dnaGame.py
@@ -1,4 +1,4 @@
-# DNA replication Gavin Kloeckner, v0.2a
+# DNA replication Gavin Kloeckner, v0.3a
 
 import time, datetime # Bring the whole tool box
 
@@ -21,5 +21,19 @@ def genDNA() -> str:
         basesGenerated += 1
     return dnaSequence
 
-# dna = genDNA()
-# print(dna)
+def genRNA(dnaSequence: str) -> tuple:
+    print(f"Your DNA sequence is {dnaSequence}.\n")
+    print("You need to enter the correct RNA Sequence based on this DNA Sequence.\n")
+    print("Remember, the RNA base will match U with A.\n")
+    # Start Timer
+    rnaStart = time.time()
+    rnaSequence = input("Please type the correct RNA sequence with no spaces.\n")
+    rnaStop = time.time()
+    rnaTime = rnaStop - rnaStart
+    return(rnaSequence, rnaTime) # Tuples are ordered (index), unchangeable, allows duplicates
+
+dna = genDNA()
+print(dna)
+
+rna = genRNA(dna)
+print(rna)
