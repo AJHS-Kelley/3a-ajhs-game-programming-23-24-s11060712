cfedbc2d5ff561db6715a6a640821d190b09491f
s11060712 <111775682+s11060712@users.noreply.github.com>
2024-01-23 11:45:40 -0500

    v0.0

---
+++ gameProgramming 3A/computer_science_exercises/07_numberSlide/numberSlide.py
@@ -0,0 +1,12 @@
+# Number Slide, Gavin Kloeckner, based on a project by Al Sweiggart, v0.0
+
+# Game Setup / Steps
+# Function to Layout / Setup the Grid
+# Create the tiles and add them to the board
+# Check for valid movements
+# Animate movement of tile
+# Allow for user input to select tile (mouse click)
+# Identify location of coordinates of mouse click, determine tile selected
+# Check for win -- All numbers in the correct order
+
+# Divide and Conquer Method -- Break down a larger problem into smaller problems, then solve those problems
