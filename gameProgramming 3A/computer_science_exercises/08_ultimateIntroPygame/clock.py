0a34d7d05e7d1c0b9989c39c060f466f3b3bec7b
s11060712 <s11060712@students.duvalschools.org>
2024-01-29 13:19:37 -0500

    v0.0.1

--- gameProgramming 3A/computer_science_exercises/08_ultimateIntroPygame/clock.py
+++ gameProgramming 3A/computer_science_exercises/08_ultimateIntroPygame/clock.py
@@ -1,1 +1,14 @@
-# Displaying Images on a Surface, Gavin Kloeckner, v0.0
+# Displaying Images on a Surface, Gavin Kloeckner, v0.0.1
+
+import pygame
+
+pygame.init
+screen = pygame.display.set_mode((890,410))
+
+while True:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            pygame.quit()
+    # draw all of our elements
+    # update everything
+    pygame.display.update()
