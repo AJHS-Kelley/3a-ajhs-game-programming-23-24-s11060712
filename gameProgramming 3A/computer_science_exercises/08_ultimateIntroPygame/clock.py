8538e72dee023b91daf2449c616cf47028488875
s11060712 <s11060712@students.duvalschools.org>
2024-01-31 12:00:46 -0500

    v0.0.4

--- gameProgramming 3A/computer_science_exercises/08_ultimateIntroPygame/clock.py
+++ gameProgramming 3A/computer_science_exercises/08_ultimateIntroPygame/clock.py
@@ -1,4 +1,4 @@
-# Displaying Images on a Surface, Gavin Kloeckner, v0.0.3
+# Displaying Images on a Surface, Gavin Kloeckner, v0.0.4
 
 import pygame
 from sys import exit
@@ -13,6 +13,8 @@ ground_surface = pygame.image.load('graphics/red_stone.jpg')
 sky_surface = pygame.image.load('graphics/blue_sky.jpg')
 text_surface = test_font.render('My Game', True, 'Dark Green')
 
+alligator_surface = pygame.image.load('graphics/Alligator.jpg')
+
 while True:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
@@ -22,6 +24,7 @@ while True:
     screen.blit(sky_surface, (0, 0))
     screen.blit(ground_surface, (0, 300))
     screen.blit(text_surface, (300, 50))
+    screen.blit(alligator_surface, (550, 250))
 
     pygame.display.update()
     clock.tick(60)
