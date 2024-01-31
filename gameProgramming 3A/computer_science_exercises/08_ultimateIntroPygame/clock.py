ce94e3fae501619b1af8e46b74da52dbf27a4a7a
s11060712 <s11060712@students.duvalschools.org>
2024-01-31 10:35:45 -0500

    v0.0.2

--- gameProgramming 3A/computer_science_exercises/08_ultimateIntroPygame/clock.py
+++ gameProgramming 3A/computer_science_exercises/08_ultimateIntroPygame/clock.py
@@ -1,14 +1,22 @@
-# Displaying Images on a Surface, Gavin Kloeckner, v0.0.1
+# Displaying Images on a Surface, Gavin Kloeckner, v0.0.2
 
 import pygame
 
 pygame.init
 screen = pygame.display.set_mode((890,410))
+pygame.display.set_caption('Runner')
+clock = pygame.time.Clock()
+
+test_surface = pygame.Surface((840, 100))
+test_surface.fill('Red')
 
 while True:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             pygame.quit()
-    # draw all of our elements
-    # update everything
+            exit()
+
+    screen.blit(test_surface, (0, 300))
+
     pygame.display.update()
+    clock.tick(60)
