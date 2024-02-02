631d2fc8d601d56a41d60a172eeea870ae7f5db9
s11060712 <s11060712@students.duvalschools.org>
2024-02-02 12:04:21 -0500

    v0.0.6

--- gameProgramming 3A/computer_science_exercises/08_ultimateIntroPygame/clock.py
+++ gameProgramming 3A/computer_science_exercises/08_ultimateIntroPygame/clock.py
@@ -1,4 +1,4 @@
-# Displaying Images on a Surface, Gavin Kloeckner, v0.0.5
+# Displaying Images on a Surface, Gavin Kloeckner, v0.0.6
 
 import pygame
 from sys import exit
@@ -9,12 +9,17 @@ pygame.display.set_caption('Runner')
 clock = pygame.time.Clock()
 test_font = pygame.font.Font(None, 50)
 
-ground_surface = pygame.image.load('graphics/red_stone.jpg')
-sky_surface = pygame.image.load('graphics/blue_sky.jpg')
+ground_surface = pygame.image.load('graphics/red_stone.jpg').convert()
+sky_surface = pygame.image.load('graphics/blue_sky.jpg').convert()
 text_surface = test_font.render('My Game', True, 'Dark Green')
 
-alligator_surface = pygame.image.load('graphics/Alligator.jpg')
+alligator_surface = pygame.image.load('graphics/Alligator.png').convert_alpha()
+alligator_rect = alligator_surface.get_rect(bottomright = (600, 300))
 alligator_x_pos = 800
+
+player_surface = pygame.image.load('graphics/steve.png').convert_alpha()
+player_x_pos = 200
+player_rect = player_surface.get_rect(midbottom = (50,300))
 
 while True:
     for event in pygame.event.get():
@@ -27,7 +32,8 @@ while True:
     screen.blit(text_surface, (300, 50))
     alligator_x_pos -= 3
     if alligator_x_pos < -300: alligator_x_pos = 800
-    screen.blit(alligator_surface, (alligator_x_pos, 250))
+    screen.blit(alligator_surface, alligator_rect)
+    screen.blit(player_surface, player_rect)
 
     pygame.display.update()
     clock.tick(80)
