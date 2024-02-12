17de41c5a191565825e2b6049d1f721ed41961a4
s11060712 <s11060712@students.duvalschools.org>
2024-02-12 11:42:47 -0500

    v0.1.1

--- gameProgramming 3A/computer_science_exercises/08_ultimateIntroPygame/clock.py
+++ gameProgramming 3A/computer_science_exercises/08_ultimateIntroPygame/clock.py
@@ -1,4 +1,4 @@
-# Displaying Images on a Surface, Gavin Kloeckner, v0.1.0
+# Displaying Images on a Surface, Gavin Kloeckner, v0.1.1
 
 import pygame
 from sys import exit
@@ -8,6 +8,7 @@ screen = pygame.display.set_mode((800,400))
 pygame.display.set_caption('Runner')
 clock = pygame.time.Clock()
 test_font = pygame.font.Font(None, 50)
+gameActive = True
 
 ground_surf = pygame.image.load('graphics/red_stone.jpg').convert()
 sky_surf = pygame.image.load('graphics/blue_sky.jpg').convert()
@@ -29,40 +30,53 @@ while True:
         if event.type == pygame.QUIT:
             pygame.quit()
             exit()
-        # if event.type == pygame.MOUSEBUTTONDOWN:
-            # if player_rect.collidepoint(event.pos):
-            #     player_gravity = -20
+
+        if gameActive:
+            if event.type == pygame.MOUSEBUTTONDOWN:
+                if player_rect.collidepoint(event.pos) and player_rect.bottom >= 300:
+                    player_gravity = -10
             
-        if event.type == pygame.KEYDOWN:
-            if event.key == pygame.K_SPACE:
-                player_gravity = -20 
+            if event.type == pygame.KEYDOWN:
+                if event.key == pygame.K_SPACE and player_rect.bottom >= 300:
+                    player_gravity = -10
+            
+        else:
+            if event.type == pygame.KEYDOWN and event.key == pygame.K_SPACE:
+                gameActive = True
+                alligator_rect.left = 800
 
 
 
+    if gameActive:
+        screen.blit(sky_surf, (0, 0))
+        screen.blit(ground_surf, (0, 300))
+        pygame.draw.rect(screen, 'Green', score_rect, 17, 15)
+        screen.blit(score_surf, score_rect)
 
-    screen.blit(sky_surf, (0, 0))
-    screen.blit(ground_surf, (0, 300))
-    pygame.draw.rect(screen, 'Green', score_rect, 17, 15)
-    screen.blit(score_surf, score_rect)
+        alligator_rect.x -= 3
+        if alligator_rect.right <= 0: alligator_rect.left = 800
+        screen.blit(alligator_surf, alligator_rect)
 
-    alligator_rect.x -= 3
-    if alligator_rect.right <= 0: alligator_rect.left = 800
-    screen.blit(alligator_surf, alligator_rect)
+        player_gravity += 0.2
+        player_rect.y += player_gravity
+        if player_rect.bottom >= 300: player_rect.bottom = 300
+        screen.blit(player_surf, player_rect)
 
-    player_gravity += 1
-    player_rect.y += player_gravity
-    screen.blit(player_surf, player_rect)
+        # keys = pygame.key.get_pressed()
+        # if keys[pygame.K_SPACE]:
+        #     print('jump ')
 
-    # keys = pygame.key.get_pressed()
-    # if keys[pygame.K_SPACE]:
-    #     print('jump ')
-
-    # if player_rect.colliderect(alligator_rect):
-    #     print('Collision Detected!')
+        # if player_rect.colliderect(alligator_rect):
+        #     print('Collision Detected!')
     
-    # mouse_pos = pygame.mouse.get_pos()
-    # if player_rect.collidepoint((mouse_pos)):
-    #     print(pygame.mouse.get_pressed())
+        # mouse_pos = pygame.mouse.get_pos()
+        # if player_rect.collidepoint((mouse_pos)):
+        #     print(pygame.mouse.get_pressed())
+
+        if alligator_rect.colliderect(player_rect):
+            gameActive = False
+    else:
+        screen.fill('Red')
 
 
     pygame.display.update()
