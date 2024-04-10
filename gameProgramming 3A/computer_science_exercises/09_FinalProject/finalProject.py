2946954f0675e326b64a6993005f8f80ece1a1fc
s11060712 <s11060712@students.duvalschools.org>
2024-04-10 12:05:52 -0400

    Final Project v0.1

--- gameProgramming 3A/computer_science_exercises/09_FinalProject/finalProject.py
+++ gameProgramming 3A/computer_science_exercises/09_FinalProject/finalProject.py
@@ -1,20 +1,28 @@
-# Ariel Combat Sim, Gavin Kloeckner, v0.0
+# Ariel Combat Sim, Gavin Kloeckner, v0.1
 
 import pygame
 from sys import exit
 from random import randint   
 
 pygame.init()
-screen = pygame.display.set_mode((800,400))
-pygame.display.set_caption('Runner')
+screen = pygame.display.set_mode((1000,900))
+pygame.display.set_caption('Soar')
 clock = pygame.time.Clock()
-test_font = pygame.font.Font(None, 50)
-gameActive = False
-startTime = 0
-sky = 0
+screen.fill('Light Blue')
+test_font = pygame.font.Font(None, 198)
+
+text_surface = test_font.render('Soar', False, 'Green')
+
+mig_surface = pygame.image.load()
+
+while True:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            pygame.quit()
+            exit()
+    pygame.display.update()
+    clock.tick(90)
+
+    screen.blit(text_surface, (340,290))
 
 
-if sky == 0:
-    sky_surf = pygame.image.load
-else:
-    sky_surf = pygame.image.load
