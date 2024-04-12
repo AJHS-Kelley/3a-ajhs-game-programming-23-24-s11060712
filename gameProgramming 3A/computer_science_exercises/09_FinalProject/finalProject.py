7372ce9642c7142d3f7a9015c898a41e5bc3f13c
s11060712 <s11060712@students.duvalschools.org>
2024-04-12 11:57:40 -0400

    v0.1.1

--- gameProgramming 3A/computer_science_exercises/09_FinalProject/finalProject.py
+++ gameProgramming 3A/computer_science_exercises/09_FinalProject/finalProject.py
@@ -1,4 +1,4 @@
-# Ariel Combat Sim, Gavin Kloeckner, v0.1
+# Arial Combat Sim, Gavin Kloeckner, v0.1
 
 import pygame
 from sys import exit
@@ -6,23 +6,42 @@ from random import randint
 
 pygame.init()
 screen = pygame.display.set_mode((1000,900))
-pygame.display.set_caption('Soar')
+pygame.display.set_caption('Arial Combat Sim')
 clock = pygame.time.Clock()
 screen.fill('Light Blue')
-test_font = pygame.font.Font(None, 198)
+test_font = pygame.font.Font(None, 87)
+test_font2 = pygame.font.Font(None, 67)
+test_font3 = pygame.font.Font(None, 47)
 
-text_surface = test_font.render('Soar', False, 'Green')
+gameName = test_font.render('Arial Combat Sim', False, 'Dark Gray')
+gameName_rect = gameName.get_rect(center = (490, 130))
+gameMessage = test_font2.render('Press the UP arrow to run the game', False, 'Dark Gray')
+gameMessage_rect = gameMessage.get_rect(center = (490, 270))
+gameMessage2 = test_font3.render('A Game by Gavin Kloeckner', False, 'Dark Green')
+gameMessage2_rect = gameMessage2.get_rect(center =(490, 210))
 
-mig_surface = pygame.image.load()
+gameActive = True
+
+
+# mig_surface = pygame.image.load()
+
 
 while True:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             pygame.quit()
             exit()
+
     pygame.display.update()
     clock.tick(90)
 
-    screen.blit(text_surface, (340,290))
+
+    screen.blit(gameName, gameName_rect)
+    screen.blit(gameMessage, gameMessage_rect)
+    screen.blit(gameMessage2, gameMessage2_rect)
+
+
+
+
 
 
